29558fadeeb0b6a94524d696c2afa0137cb6e9a0
navincograph179 <navin_gurung@co-graph.com>
2024-09-03 15:51:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { users } from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [ searchItem, setSearchItem ] = useState('');
+  const [ filteredUsers, setFilteredUsers ] = useState(users);
+
+
+  const handleClick = () => {
+    const filterItems = users.filter(
+      (user) => user.firstName.toLowerCase().includes(searchItem.toLowerCase())
+    )
+
+    const filterItemsAge = users.filter(
+      (user) => user.age.toString().includes(searchItem)
+    )
+
+    if (filterItems.length !== 0) {
+      setFilteredUsers(filterItems);
+    }
+    
+    if (filterItemsAge.length !== 0) {
+      setFilteredUsers(filterItemsAge);
+    }
+
+    // setFilteredUsers(filterItems);
+  }
+  const handleInputChange = (e) => {
+    const inputTerm = e.target.value;
+    setSearchItem(inputTerm);
+  }
+return (
+<div className='container'>
+  <input 
+  className='search-input'
+  placeholder='search items'
+  type='text'
+  onChange={handleInputChange}
+  />
+  <button
+  onClick={handleClick}
+  >Search</button>
+
+  <ul>
+    {filteredUsers.map(user => <li key={user.id}>{`${user.firstName} - ${user.age}`}</li>)}
+  </ul>
+</div>
+)
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const users = [
+  { firstName: "John", age: 45, id: 1 },
+  { firstName: "Emily", age: 45, id: 2 },
+  { firstName: "Michael", age: 665, id: 3 },
+  { firstName: "Sarah", age: 95, id: 4 },
+  { firstName: "David", age: 75, id: 5 },
+  { firstName: "Jessica", age: 65, id: 6 },
+  { firstName: "Daniel", age: 5, id: 7 },
+  { firstName: "Olivia", age: 4, id: 8 },
+  { firstName: "Matthew", age: 45, id: 9 },
+  { firstName: "Sophia", age: 25, id: 10 },
+];
