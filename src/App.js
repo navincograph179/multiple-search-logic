0884177ebc75d043d4d5c077bac6196391eb68b9
navincograph179 <navin_gurung@co-graph.com>
2024-09-03 16:26:54 +0900

    some comments added

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
-import React, { useState } from 'react';
-import './App.css';
-import { users } from './data'
+import React, { useState } from "react";
+import "./App.css";
+import { users } from "./data";
 
 const App = () => {
-  const [ searchItem, setSearchItem ] = useState('');
-  const [ filteredUsers, setFilteredUsers ] = useState(users);
+  const [searchItem, setSearchItem] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
+  // const filteredUsers = users.filter((user) =>
+  //   Object.values(user).some((value) =>
+  //     value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+  //   )
+  // );
+  //The above code is optional or shortcut for these projects
 
   const handleClick = () => {
-    const filterItems = users.filter(
-      (user) => user.firstName.toLowerCase().includes(searchItem.toLowerCase())
-    )
+    const filterItems = users.filter((user) =>
+      user.firstName.toLowerCase().includes(searchItem.toLowerCase())
+    );
 
-    const filterItemsAge = users.filter(
-      (user) => user.age.toString().includes(searchItem)
-    )
+    const filterItemsAge = users.filter((user) =>
+      user.age.toString().includes(searchItem)
+    );
 
     if (filterItems.length !== 0) {
       setFilteredUsers(filterItems);
     }
-    
+
     if (filterItemsAge.length !== 0) {
       setFilteredUsers(filterItemsAge);
     }
 
     // setFilteredUsers(filterItems);
-  }
+  };
   const handleInputChange = (e) => {
     const inputTerm = e.target.value;
     setSearchItem(inputTerm);
-  }
-return (
-<div className='container'>
-  <input 
-  className='search-input'
-  placeholder='search items'
-  type='text'
-  onChange={handleInputChange}
-  />
-  <button
-  onClick={handleClick}
-  >Search</button>
+  };
+  return (
+    <div className="container">
+      <input
+        className="search-input"
+        placeholder="search items"
+        type="text"
+        onChange={handleInputChange}
+      />
+      <button onClick={handleClick}>Search</button>
 
-  <ul>
-    {filteredUsers.map(user => <li key={user.id}>{`${user.firstName} - ${user.age}`}</li>)}
-  </ul>
-</div>
-)
-}
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>{`${user.firstName} - ${user.age}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
